db5b736f6386c095be78de66a259127f724af8d9
Devin <devinbalian@gmail.com>
2023-04-19 22:28:20 +0200

    übung 1 b)

---
+++ exercise1/main.m
@@ -0,0 +1,30 @@
+clear all;
+
+N = 15;
+delta = 1e-5;
+e = zeros(N, 1);
+cond = zeros(N, 1);
+for n = 1:N
+
+    % Hilbert matrix
+    H = hilb(n);
+    Hinv = invhilb(n);
+
+    % rhs vector
+    b = ones(n, 1);
+    b_delta = b + rand(n, 1) * delta / sqrt(n);
+    e_b = norm(b - b_delta)/norm(b);
+
+    x = H\b;
+    x_delta = H\b_delta;
+    e_x = norm(x - x_delta)/norm(x);
+
+    % Set error and data
+    e(n) = e_x/e_b;
+    cond(n) = norm(H)*norm(Hinv);
+end
+
+semilogy(e)
+hold on
+semilogy(cond)
+legend("e_x/e_b", "cond")
