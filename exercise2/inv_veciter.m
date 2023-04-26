d041f39bbfa7587d2c2696e4c036b41b83fd8e3c
Devin <devinbalian@gmail.com>
2023-04-26 17:22:43 +0200

    updated inv_veciter.m

--- exercise2/inv_veciter.m
+++ exercise2/inv_veciter.m
@@ -4,15 +4,14 @@ function [lambda, x] = inv_veciter(A, mu, x0, kmax)
     lambda = zeros(1,kmax);
 
     % Init lambda and x
-    x(:,1) = x0;
+    x(:,1) = x0 / norm(x0);
     lambda(1) = mu;
-    y = x0 / norm(x0);
     [L,U,P] = lu(A - mu);
 
     for k = 2:kmax
-        x_k = U\(L\(P*y));
+        x_k = U\(L\(P*x(:,k-1)));
+        x_k = x_k / norm(x_k);
         x(:,k) = x_k;
-        y = x_k / norm(x_k);
-        lambda(k) = dot(y, A*y);
+        lambda(k) = dot(x_k, A*x_k);
     end
 end
