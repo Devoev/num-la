10adcd808b6c9a0ebbd6c0262757ad03c074b460
Devin <devinbalian@gmail.com>
2023-04-26 17:44:40 +0200

    finished exercise 2

--- exercise2/main.m
+++ exercise2/main.m
@@ -8,6 +8,29 @@ mu = 3;
 x0 = [1;1;1];
 kmax = 15;
 
-[lambda, x] = inv_veciter(A, mu, x0, kmax)
+% Inverse vector iteration
+[lambda, x] = inv_veciter(A, mu, x0, kmax);
 
-[V, D] = eig(A)
+% "exact" eigenvalues/vectors
+[V, D] = eig(A);
+lambda1 = D(1,1);
+lambda2 = D(2,2);
+lambda3 = D(3,3);
+x2 = V(:,2);
+
+% Calculate errors
+k_range = 1:kmax;
+e_lambda = abs(lambda2 - lambda);
+e_vec = arrayfun(@(k) min( norm(x2 - x(:,k), Inf) , norm(x2 + x(:,k), Inf) ), k_range);
+
+% Additional lines
+c1 = power(abs((lambda2 - mu) / (lambda3 - mu)), 2*k_range);
+c2 = power(abs((lambda2 - mu) / (lambda1 - mu)), 2*k_range);
+c3 = power(abs((lambda2 - mu) / (lambda3 - mu)), k_range);
+c4 = power(abs((lambda2 - mu) / (lambda1 - mu)), k_range);
+
+% Plot errors
+plot(k_range, e_lambda, k_range, e_vec, k_range, c1, k_range, c2, k_range, c3, k_range, c4)
+xlabel("k")
+title("Inverse vector iteration convergence analysis")
+legend("Absolute error (2nd eigenvalue)", "Inf norm error (2nd eigenvector)", "c1(k)", "c2(k)", "c3(k)", "c4(k)")
