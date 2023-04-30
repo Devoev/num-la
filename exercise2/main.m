71938ca93734608b14bc10ffa521f9b7f0f99860
Devin <devinbalian@gmail.com>
2023-04-30 23:10:17 +0200

    updated plot to semilogy

--- exercise2/main.m
+++ exercise2/main.m
@@ -30,7 +30,7 @@ c3 = power(abs((lambda2 - mu) / (lambda3 - mu)), k_range);
 c4 = power(abs((lambda2 - mu) / (lambda1 - mu)), k_range);
 
 % Plot errors
-plot(k_range, e_lambda, k_range, e_vec, k_range, c1, k_range, c2, k_range, c3, k_range, c4)
+semilogy(k_range, e_lambda, k_range, e_vec, k_range, c1, k_range, c2, k_range, c3, k_range, c4)
 xlabel("k")
 title("Inverse vector iteration convergence analysis")
 legend("Absolute error (2nd eigenvalue)", "Inf norm error (2nd eigenvector)", "c1(k)", "c2(k)", "c3(k)", "c4(k)")
