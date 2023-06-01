01c0009ca240ca351edec744e717aa569bfb1847
Devin <devinbalian@gmail.com>
2023-06-01 18:51:11 +0200

    implemented shifts

--- exercise4/main.m
+++ exercise4/main.m
@@ -4,6 +4,10 @@ clear all;
 %%% Exercise a) iii)
 A = [1, 2;
     2, 3];
-kmax = 2000;
+kmax = 2;
 
-ev = qralgorithm(A, 'abcd', kmax)
+ev_num = qralgorithm(A, 'wilkinson', kmax);
+ev_exact = eig(A);
+
+ev_error = norm(sort(ev_num) - sort(ev_exact)) / norm(ev_exact);
+disp(ev_error)
--- exercise4/qralgorithm.m
+++ exercise4/qralgorithm.m
@@ -3,6 +3,7 @@
 function [ev] = qralgorithm(A, shift, kmax)
     % Transformation to hessenberg form
     H = hess(A);
+    [n,~] = size(H);
 
     % QR Iteration
     for k = 1:kmax
@@ -10,15 +11,16 @@ function [ev] = qralgorithm(A, shift, kmax)
         if strcmp(shift, 'none')
             sigma = 0;
         elseif strcmp(shift, 'naive')
-            sigma = H(end, end);
+            sigma = H(n, n);
         elseif strcmp(shift, 'wilkinson')
-            sigma = None;
+            d = (H(n-1,n-1) - H(n,n)) / 2;
+            sigma = H(n,n) + d - sign(d)*sqrt(d^2 + H(n-1,n)^2);
         else
             error("Unsupported shift routine. Use 'none', 'naive' or 'wilkinson'.");
         end
 
-        [Q, R]= qr(H);
-        H = R*Q;
+        [Q, R]= qr(H - sigma*eye(n));
+        H = R*Q + sigma*eye(n);
     end
 
     % Get diagonal elements of A
