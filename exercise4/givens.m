3d441ad9709f2cab89d3c27509af87d623b6a9fe
Devin <devinbalian@gmail.com>
2023-06-01 19:19:03 +0200

    first implementation of givens.m

--- exercise4/givens.m
+++ exercise4/givens.m
@@ -1,6 +1,17 @@
-function [G] = givens(A, i, j)
-% GIVENS Calculates the givens matrix for A that sets the element A(i,j) to zero.
+function [G] = givens(A, p, q, j)
+% GIVENS Calculates the givens matrix for A that sets the element A(q,j) to zero.
 
+    [n, ~] = size(A);
+    G = eye(n);
+    xp = A(p,j);
+    xq = A(q,j);
+    r = sqrt(xp^2 + xq^2);
+    c = xp/r;
+    s = xq/r;
+    G(p,p) = c;
+    G(q,q) = c;
+    G(p,q) = s;
+    G(q,p) = -s;
 end
 
 % Devin Balian 2791430
--- exercise4/main.m
+++ exercise4/main.m
@@ -6,6 +6,10 @@ A = [1, 2;
     2, 3];
 kmax = 2;
 
+M = magic(5)
+G = givens(M, 1, 2, 2)
+G * M
+
 ev_num = qr_algorithm(A, 'wilkinson', kmax);
 ev_exact = eig(A);
 
