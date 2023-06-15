873357a81a71e9e841976533f1f6e20e8b2e03a9
Devin <devinbalian@gmail.com>
2023-06-15 16:41:16 +0200

    added cat-space.gif updated plots

--- exercise5/main.m
+++ exercise5/main.m
@@ -1,16 +1,21 @@
+% Devin Balian 2791430
+
 % Parse flower.gif
-[X,map]=imread("flower.gif");
+[X,map]=imread("cat-space.gif");
 A=im2double(X,"indexed");
 
 % Calculate SVD and low rank apprximation.
-t = 500;
+t = 220;
 [U,S,V] = svd(A);
 At = low_rank(U,S,V,t);
 
 % Show images
+subplot(1,2,1)
 imshow(A,map)
-figure
+title("Original image")
+subplot(1,2,2)
 imshow(At,map)
+title("Low rank approximation of rank " + t)
 
 % Plot singular values
 figure
