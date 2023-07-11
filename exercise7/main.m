3755ff126baf5c3bb232684170857ae9162571e7
Devin <devinbalian@gmail.com>
2023-07-11 18:18:17 +0200

    added disp

--- exercise7/main.m
+++ exercise7/main.m
@@ -16,10 +16,12 @@ maxit = 5000;
 
 [x1,k1,resvec1] = cg(A1,b1,x01,tol,maxit);
 [x2,k2,resvec2] = cg(A2,b2,x02,tol,maxit);
+disp("Matrix of size (n,m) = (100,10) converges after k=" + k1 + " iterations.")
+disp("Matrix of size (n,m) = (1000,100) converges after k=" + k2 + " iterations.")
 
 %% c)
 loglog(1:k1, resvec1)
-title('CG convergence study for $(n,m) = (1000,100)$', 'Interpreter', 'Latex')
+title('CG convergence study for $(n,m) = (100,10)$', 'Interpreter', 'Latex')
 xlabel('Iteration number $k$', 'Interpreter', 'Latex')
 ylabel('Absolute $L^2$ error $||\mathbf{Ax}^{(k)} - \mathbf{b}||_2$', 'Interpreter', 'Latex')
 legend("Absolute residual error")
@@ -27,7 +29,7 @@ grid on
 
 figure
 loglog(1:k2, resvec2)
-title('CG convergence study for $(n,m) = (100,10)$', 'Interpreter', 'Latex')
+title('CG convergence study for $(n,m) = (1000,100)$', 'Interpreter', 'Latex')
 xlabel('Iteration number $k$', 'Interpreter', 'Latex')
 ylabel('Absolute $L^2$ error $||\mathbf{Ax}^{(k)} - \mathbf{b}||_2$', 'Interpreter', 'Latex')
 legend("Absolute residual error")
