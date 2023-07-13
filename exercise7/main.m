bd0342bef0e111c140c1e3081d2de1c3dd30edc2
Devin <devinbalian@gmail.com>
2023-07-13 18:14:01 +0200

    task 7 update

--- exercise7/main.m
+++ exercise7/main.m
@@ -13,6 +13,7 @@ maxit = 5000;
 
 [~,k1,resvec1] = cg(A1,b1,x01,tol,maxit);
 [~,k2,resvec2] = cg(A2,b2,x02,tol,maxit);
+disp("task a)")
 disp("Matrix of size (n,m) = (100,10) converges after k=" + k1 + " iterations.")
 disp("Matrix of size (n,m) = (1000,100) converges after k=" + k2 + " iterations.")
 
@@ -21,10 +22,24 @@ P1 = diag(diag(A1));
 P2 = diag(diag(A2));
 [~,k1p,resvec1p] = pcg(A1,b1,P1,x01,tol,maxit);
 [~,k2p,resvec2p] = pcg(A2,b2,P2,x02,tol,maxit);
+disp("task b)")
 disp("Preconditioned matrix of size (n,m) = (100,10) converges after k=" + k1p + " iterations.")
 disp("Preconditioned matrix of size (n,m) = (1000,100) converges after k=" + k2p + " iterations.")
 
 %% c)
+c1 = condest(A1);
+c1p = condest(inv(P1)*A1);
+c2 = condest(A2);
+c2p = condest(inv(P2)*A2);
+
+disp("task c)")
+disp("Condition of matrix of size (n,m) = (100,10):")
+disp("  cond(A) = " + c1);
+disp("  cond(P^-1 A) = " + c1p);
+disp("Condition of matrix of size (n,m) = (1000,100):")
+disp("  cond(A) = " + c2);
+disp("  cond(P^-1 A) = " + c2p);
+
 loglog(1:k1, resvec1)
 hold on
 loglog(1:k1p, resvec1p)
